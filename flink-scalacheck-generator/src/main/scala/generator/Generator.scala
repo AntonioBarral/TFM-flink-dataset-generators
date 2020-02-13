50a2b83eff31285a01f2ca0c4bf57a4d8a426f13
AntonioBarral <anbarr02@ucm.es>
2020-02-13 18:48:34 +0100

    :sparkles: Added paralelism and assigned each Dataset to the correct partition (in theory, need to check in real distributed environment

--- flink-scalacheck-generator/src/main/scala/generator/Generator.scala
+++ flink-scalacheck-generator/src/main/scala/generator/Generator.scala
@@ -1,25 +1,38 @@
 package generator
 
 import org.apache.flink.api.scala._
+import org.apache.flink.streaming.api.scala.KeyedStream
 import org.scalacheck.Gen
 
 
 object Generator {
 
-  def generateDatasetGenerator(numElements: Int, numPartitions: Int, g: Gen[Int]): Gen[DataSet[Int]] = {
-    // set up the execution environment
-    val env = ExecutionEnvironment.getExecutionEnvironment
-    //env.fromCollection(Seq(1,2,3,4,5,6))
-    env.fromCollection(Gen.listOfN(numElements, g))
-    env.setParallelism(numPartitions)
+  def generateDataSetGenerator(numElements: Int, numPartitions: Int, g: Gen[Int]) (implicit env: ExecutionEnvironment): DataSet[Int]= {
+    env.fromElements(Gen.listOfN(numElements, g))
+    val indexes : DataSet[(Int, Int)] = env.fromElements((1 to numPartitions):_*)
+      .map(xs => (xs,xs)) //Create a tuple dataset. First element is partition number. Second is trivial now. Could be the seed in the future
+
+    val finalDataSet: DataSet[Int] = indexes
+        .partitionByRange(0) //Send each list to the partition with value equal to first position of the tuple
+        .flatMap{_ =>
+          val element: List[Int] = Gen.listOfN(numElements, g).sample.getOrElse(Nil)
+          element
+        }
+
+    print(finalDataSet.print())
+    finalDataSet
   }
+
 
   def main(args: Array[String]): Unit = {
     //Intento de funcion que me piden
+    implicit val env = ExecutionEnvironment.getExecutionEnvironment
     val numPartitions = 4
-    val numElements = 100000
-    val genVar = Gen.choose(1, 200000) // -> Gen[Int]
+    val numElements = 100
+    val genVar = Gen.choose(1, 20) // -> Gen[Int]
 
-    generateDatasetGenerator(numElements, numPartitions, genVar)
+    env.setParallelism(numPartitions) // Create a parallelism level equal to the num of partitions we want to create
+
+    generateDataSetGenerator(numElements, numPartitions, genVar)
   }
 }
