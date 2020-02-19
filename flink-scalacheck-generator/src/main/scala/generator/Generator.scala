064a52e50e651f8f256dec54bfb1b6668ae9c1b5
AntonioBarral <anbarr02@ucm.es>
2020-02-19 17:38:48 +0100

    :tada: Program is load balanced

--- flink-scalacheck-generator/src/main/scala/generator/Generator.scala
+++ flink-scalacheck-generator/src/main/scala/generator/Generator.scala
@@ -1,39 +1,43 @@
 package generator
 
+import org.apache.flink.api.common.typeinfo.TypeInformation
 import org.apache.flink.api.scala._
 import org.apache.flink.streaming.api.scala.KeyedStream
 import org.scalacheck.Gen
 
+import scala.reflect.ClassTag
+
 
 object Generator {
 
-  def generateDataSetGenerator(numElements: Int, numPartitions: Int, g: Gen[Int]) (implicit env: ExecutionEnvironment): DataSet[Int]= {
+  def generateDataSetGenerator[A: ClassTag : TypeInformation](numElements: Int, numPartitions: Int, g: Gen[A])(implicit env: ExecutionEnvironment): Gen[DataSet[A]] = {
     env.fromElements(Gen.listOfN(numElements, g))
-    val indexes : DataSet[(Int, Int)] = env.fromElements((1 to numPartitions):_*)
-      .map(xs => (xs,xs)) //Create a tuple dataset. First element is partition number. Second is trivial now. Could be the seed in the future
+    val indexes: DataSet[(Int, Int)] = env.fromElements((1 to numPartitions): _*)
+      .map(xs => (xs, xs)) //Create a tuple dataset. First element is partition number. Second is trivial now. Could be the seed in the future
 
-    val finalDataSet: DataSet[Int] = indexes
-        .partitionByRange(0) //Send each list to the partition with value equal to first position of the tuple
-        .flatMap{_ =>
-          val element: List[Int] = Gen.listOfN(numElements, g).sample.getOrElse(Nil)
-          element
-        }
+    val finalDataSet: DataSet[A] = indexes
+      .partitionByRange(0) //Send each list to the partition with value equal to first position of the tuple
+      .flatMap { _ =>
+        val element: List[A] = Gen.listOfN(numElements, g).sample.getOrElse(Nil)
+        element
+      }
+      .setParallelism(numPartitions)
 
-    //print(finalDataSet.print())
     finalDataSet
   }
 
 
   def main(args: Array[String]): Unit = {
-    //Intento de funcion que me piden
+    //Starts program. Set needed values
     implicit val env = ExecutionEnvironment.getExecutionEnvironment
-    val numPartitions = 4
-    val numElements = 1000000
+    val numPartitions = 3
+    val numElements = 10000
     val genVar = Gen.choose(1, 20) // -> Gen[Int]
 
-    env.setParallelism(numPartitions) // Create a parallelism level equal to the num of partitions we want to create
+    //env.setParallelism(numPartitions) // Create a parallelism level equal to the num of partitions we want to create
 
-    val gen_dataset : DataSet[Int] = generateDataSetGenerator(numElements, numPartitions, genVar)
-    print(gen_dataset.collect().length)
+    val gen_dataset: Gen[DataSet[Int]] = generateDataSetGenerator(numElements, numPartitions, genVar)
+    print(gen_dataset.sample.get.print())
   }
 }
+
