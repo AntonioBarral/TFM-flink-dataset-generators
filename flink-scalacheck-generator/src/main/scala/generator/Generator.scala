b3fa59a3696010790b0851f8a9f19a4b519c7b70
AntonioBarral <anbarr02@ucm.es>
2020-03-02 17:52:03 +0100

    :sparkles: Added seeds to each partition generated with a random int. Modified .gitignore

--- flink-scalacheck-generator/src/main/scala/generator/Generator.scala
+++ flink-scalacheck-generator/src/main/scala/generator/Generator.scala
@@ -2,22 +2,27 @@ package generator
 
 import org.apache.flink.api.common.typeinfo.TypeInformation
 import org.apache.flink.api.scala._
+
 import org.scalacheck.Gen
+import org.scalacheck.Gen.Parameters
+import org.scalacheck.rng.Seed
 
 import scala.reflect.ClassTag
 
 
 object Generator {
 
-  def generateDataSetGenerator[A: ClassTag : TypeInformation](numElements: Int, numPartitions: Int, g: Gen[A])(implicit env: ExecutionEnvironment): Gen[DataSet[A]] = {
+  def generateDataSetGenerator[A: ClassTag : TypeInformation](numElements: Int, numPartitions: Int, g: Gen[A])(implicit env: ExecutionEnvironment,  randomIntGen: scala.util.Random): Gen[DataSet[A]] = {
 
     val indexes: DataSet[(Int, Int)] = env.fromElements((1 to numPartitions): _*)
-      .map(xs => (xs % 3, 0)) //Create a tuple dataset. First element is partition number. Second is trivial now. Could be the seed in the future
+      .map(xs => (xs % 3, randomIntGen.nextInt())) //Create a tuple dataset. First element is partition number. Second is trivial now. Could be the seed in the future
+
+    indexes.print()
 
     val finalDataSet: DataSet[A] = indexes
       .partitionByHash(0) //Send each list to the partition with value equal to first position of the tuple
-      .flatMap { _ =>
-        val element: List[A] = Gen.listOfN(numElements, g).sample.getOrElse(Nil)
+      .flatMap { tuple =>
+        val element: List[A] = Gen.listOfN(numElements, g).apply(Parameters.default, Seed.apply(tuple._2)).getOrElse(Nil)
         element
       }
       .setParallelism(3)
@@ -33,7 +38,7 @@ object Generator {
       numPartitions -> integer (compulsory)
       numElements -> integer (compulsory)
       """
-    var numPartitions = 3
+    var numPartitions = 10
     var numElements = 100
     if (args.length == 0) {
       print(notArgsMessage)
@@ -44,12 +49,13 @@ object Generator {
       numElements = args(1).toInt
     }
 
-
     implicit val env = ExecutionEnvironment.getExecutionEnvironment
+    implicit val randomIntGenerator = scala.util.Random
+
     val genVar = Gen.choose(1, 20) // -> Gen[Int]
 
     val gen_dataset: Gen[DataSet[Int]] = generateDataSetGenerator(numElements, numPartitions, genVar)
-    print(gen_dataset.sample.get.count())
+    print(gen_dataset.sample.get.print())
   }
 }
 
