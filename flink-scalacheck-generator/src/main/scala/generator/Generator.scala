30318a141d38c8460c3a9a873f1c60fae1ff173b
AntonioBarral <anbarr02@ucm.es>
2020-03-26 17:53:14 +0100

    :wrench: Now seed is only one number passed by parameter instead of a list

--- flink-scalacheck-generator/src/main/scala/generator/Generator.scala
+++ flink-scalacheck-generator/src/main/scala/generator/Generator.scala
@@ -18,7 +18,6 @@ import scala.reflect.ClassTag
 
 object Generator {
 
-  implicit val env: ExecutionEnvironment = ExecutionEnvironment.getExecutionEnvironment
   private val extension = ".txt"
 
   class FaultTolerantSeeds[A] extends RichMapPartitionFunction[A, Int] {
@@ -58,27 +57,26 @@ object Generator {
   }
 
 
-  def generateDataSetGenerator[A: ClassTag : TypeInformation](numElements: Int, numPartitions: Int, g: Gen[A], seeds: List[Int] = List.empty[Int]): Gen[DataSet[A]] = {
-    var correctSeedPassed = true
-    var indexesList = seeds
-    if (seeds.isEmpty || seeds.size != numPartitions) { //If size is different to num partitions, it will be handled as if no seeds list was passed by parameter TODO estaria bien meter algun warning o algo avisando
-      correctSeedPassed = false
-      indexesList = 0 to numPartitions-1 toList
-    }
+  def generateDataSetGenerator[A: ClassTag : TypeInformation](numElements: Int, numPartitions: Int, g: Gen[A], seed: Option[Int] = null)
+                                                             (implicit env: ExecutionEnvironment = ExecutionEnvironment.getExecutionEnvironment): Gen[DataSet[A]] = {
 
-    val indexes: DataSet[(Int, Int)] = env.fromElements(0.until(indexesList.length): _*)
-      .map(xs => (xs, indexesList(xs))) //Create a tuple dataset, first is partition index and second is a seed if it exists
+    val indexes: DataSet[Int] = env.fromElements(0 to numPartitions-1: _*)
 
     val finalDataSet: DataSet[A] = indexes
-      .partitionByHash(0) //Send each list to the partition with value equal to first position of the tuple
-      .flatMap { tuple =>
+      .rebalance()//Send each list to the partition with value equal to first position of the tuple
+      .flatMap { xs =>
 
-        val elements: List[A] = if (correctSeedPassed) Gen.listOfN(numElements, g).apply(Parameters.default, Seed.apply(tuple._2)).getOrElse(Nil) else Gen.listOfN(numElements, g).sample.getOrElse(Nil)
+        val elements: List[A] = if (Option(seed).isDefined) Gen.listOfN(numElements, g).apply(Parameters.default, Seed.apply(seed.get)).getOrElse(Nil) else Gen.listOfN(numElements, g).sample.getOrElse(Nil)
         //println(tuple._1 + "--------------" + elements)
         elements
       }
       .setParallelism(numPartitions)
     finalDataSet
+  }
+
+  //Easy way to test generator
+  def main(args: Array[String]): Unit = {
+    print(generateDataSetGenerator(100, 3, Gen.const("antnio")).sample.get.count())
   }
 
 }
